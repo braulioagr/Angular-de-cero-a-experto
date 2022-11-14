d1ad6717ccacda1cbdaf7aa5c9f2bfee0c0f8511
Braulio Garcia <braulio.garcia@enkoder.com.mx>
2022-11-13 20:12:26 -0600

    update

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pipesApp';
-  nombre: string = 'Braulio garcia';
-  valor: number = 2404;
-  mostrarNombre(){
-    console.log( this.nombre );
-    console.log( this.valor );
+export class AppComponent implements OnInit {
+  
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
+
 }
--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
-// PrimeNG
-import { ButtonModule } from 'primeng/button';
+// Módulo personalizado
+import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-HN';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData( localeEs );
+registerLocaleData( localeFr );
 
 @NgModule({
   declarations: [
@@ -12,9 +22,14 @@ import { ButtonModule } from 'primeng/button';
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    BrowserAnimationsModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-HN' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
